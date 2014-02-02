35336ba09ece946764af55e12a1e411a97ae7d3f
croberts123 <krrobberts73@yahoo.com>
2014-02-02 09:56:37 -0600

    Added Pressed and Squeezed

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php bloginfo('name'); ?></title>
+	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
+	<script src="http://use.edgefonts.net/merriweather.js"></script>
+	<link rel="stylesheet" href="http://localhost/wp-content/themes/pressed/style.css"/>
+</head>
+	<body>
+		<header>
+			<h1>Pressed and Squeezed</h1>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">WordPress</a></li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+			<img src="http://localhost/wp-content/themes/pressed/images/hero.jpg" />
+			<section>
+				<p>Pressed and Squeezed is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
+				<p>We also make <em>smoothies</em> on the side.</p>
+			</section>
+		</section>
+		<footer>
+			<h1>Contact Us</h1>
+			<p>Pressed and Squeezed</p>
+			<p>2525 Orange Way, Orchard, CA 91104</p>
+			<p>Phone: (800) 424-3232</p>
+		</footer>
+	</body>
+</html>
